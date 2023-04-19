3de9ce8a79ee321084b5f4fc37f8ddfb5ef7e007
mmateo-t <mmateo-t@student.42madrid.com>
2023-04-19 14:54:08 +0200

    Need to check main arguments

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include "IRCServer.hpp"
 
-int main()
+void printUsage()
 {
+	std::cout << "Usage:\n\n";
+	std::cout << "./ircserv [host:port_network:password_network] <port> <password>\n";
+}
 
-	IRCServer *newSocket = new IRCServer();
-	newSocket->pollLoop();
+int main(int argc, char **argv)
+{
+/* 	if (argc > 4 || argc < 3)
+	{
+		printUsage();
+		exit(EXIT_FAILURE);
+	} */
 
+	IRCServer *irc = new IRCServer();
+	irc->pollLoop();
 	return 0;
 }
