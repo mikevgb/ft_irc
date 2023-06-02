8060f0ae1ce85d2c93ccb29e6ab9db677ef11a1a
mmateo-t <mmateo-t@student.42madrid.com>
2023-06-02 15:05:10 +0200

    feat: Add numerical replies header

--- Error.hpp
+++ Error.hpp
@@ -3,17 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   Error.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmateo-t <mmateo-t@student.42madrid>       +#+  +:+       +#+        */
+/*   By: mmateo-t <mmateo-t@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 18:47:24 by mvillaes          #+#    #+#             */
-/*   Updated: 2023/05/20 11:03:25 by mmateo-t         ###   ########.fr       */
+/*   Updated: 2023/06/02 15:04:05 by mmateo-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERROR_HPP
 #define ERROR_HPP
 #include "lib2.h"
-#include "ErrorList.h"
 
 class Error
 {
